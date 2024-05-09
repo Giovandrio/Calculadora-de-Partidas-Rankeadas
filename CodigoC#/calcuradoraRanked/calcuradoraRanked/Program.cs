bacaf62a19839b903f03d65dac9813bb6bd9939c
Giovandrio <giovanni.b.aguila@gmail.com>
2024-05-08 21:00:20 -0300

    Commit Final

---
+++ CodigoC#/calcuradoraRanked/calcuradoraRanked/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calcuradoraRanked
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantidadeVitorias = 100;
+            int quantidadeDerrotas = 15;
+
+            CalculadorRanked(quantidadeVitorias, quantidadeDerrotas);
+        }
+        public static void CalculadorRanked(int vitorias, int derrotas)
+        {
+            int saldoRanked = vitorias - derrotas;
+
+            string nivel = "";
+
+            if (saldoRanked <= 10)
+            {
+                nivel = "Ferro";
+            }
+            else if (saldoRanked <= 20)
+            {
+                nivel = "Bronze";
+            }
+            else if (saldoRanked <= 50)
+            {
+                nivel = "Prata";
+            }
+            else if (saldoRanked <= 80)
+            {
+                nivel = "Ouro";
+            }
+            else if (saldoRanked <= 90)
+            {
+                nivel = "Diamante";
+            }
+            else if (saldoRanked <= 100)
+            {
+                nivel = "Lendário";
+            }
+            else if (saldoRanked >= 101)
+            {
+                nivel = "Imortal";
+            }
+
+            Console.WriteLine("O Herói tem de saldo de {0} está no nível de {1}", saldoRanked, nivel);
+        }
+    }
+}
--- JavaScript/calculadora.js
+++ JavaScript/calculadora.js
@@ -1,1 +1,31 @@
-console.log("Hola Mundo");
+function main() {
+    const quantidadeVitorias = 100;
+    const quantidadeDerrotas = 15;
+
+    calculadorRanked(quantidadeVitorias, quantidadeDerrotas);
+}
+
+function calculadorRanked(vitorias, derrotas) {
+    const saldoRanked = vitorias - derrotas;
+    let nivel = "";
+
+    if (saldoRanked <= 10) {
+        nivel = "Ferro";
+    } else if (saldoRanked <= 20) {
+        nivel = "Bronze";
+    } else if (saldoRanked <= 50) {
+        nivel = "Prata";
+    } else if (saldoRanked <= 80) {
+        nivel = "Ouro";
+    } else if (saldoRanked <= 90) {
+        nivel = "Diamante";
+    } else if (saldoRanked <= 100) {
+        nivel = "Lendário";
+    } else if (saldoRanked >= 101) {
+        nivel = "Imortal";
+    }
+
+    console.log(`O Herói tem um saldo de ${saldoRanked} e está no nível de ${nivel}`);
+}
+
+main();
